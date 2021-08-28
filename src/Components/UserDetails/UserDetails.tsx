3dab126753cacd6d8470e2bf5843a1055e668421
unknown <binderyoni@example.com>
2021-08-28 22:45:29 +0300

    second commit

--- src/Components/UserDetails/UserDetails.tsx
+++ src/Components/UserDetails/UserDetails.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useHistory } from 'react-router-dom';
+import './UserDetails.css';
 
 export interface UserDetailsProps {
 
@@ -18,8 +19,26 @@ export default function UserDetails (props: UserDetailsProps) {
   return (
     <div>
         <form onSubmit={toFirstQuestionnaire}>
-            <h1>This is the First Slide</h1>
-            <button type="submit">Submit</button>
+            <h1>להנות כהורה ולהעביר את זה הלאה - מרכז שפר</h1>
+            <h3>
+                הורים רבים מרגישים אבודים ומתוסכלים לנוכח קשיי למידה, חיברות והתנהגות של ילדיהם.
+                <br/>
+                 ההורים עומדים חסרי אונים ולא מבינים מדוע גידול הילדים כל כך מעיק.
+                <br/>
+                  קורס ההכשרה של שפר יתן לכם כלים להפוך למנחי הורים ולתת מענה בדיוק לקשיים אלו.
+                <br/>
+                   לייעוץ והרשמה לקורס הכשרת המנחים הקרוב:
+            </h3>
+            <label>שם ושם משפחה</label>
+            <input name="fullName"/>
+            <br/>
+            <label>טלפון</label>
+            <input name="phone"/>
+            <br/>
+            <label>אימייל</label>
+            <input name="email"/>
+            <br/>
+            <button type="submit">שליחה</button>
         </form>
     </div>
   );
