ab91b880653a036e4d302a429d2dc6f50f0c416f
YoniBinder <binderyoni@gmail.com>
2021-12-20 21:53:50 +0200

    adding firebase data saving

--- src/Components/UserApp/UserApp.tsx
+++ src/Components/UserApp/UserApp.tsx
@@ -1,64 +1,70 @@
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import "./UserApp.scss";
 import { db } from "../../config/firebase";
-import { onSnapshot, doc } from "@firebase/firestore";
-import {Helmet} from 'react-helmet';
+import {
+  onSnapshot,
+  doc,
+  arrayUnion,
+  updateDoc,
+  collection,
+  addDoc,
+} from "@firebase/firestore";
+import { Helmet } from "react-helmet";
 
 interface FormComponentProps {
-  match:Match
+  match: Match;
 }
-type Params ={
-  id: string
-}
-type Match={
-  params:Params
-}
-type Colors={
-  main:string,
-  title: string,
-  background?:string,
-  description: string,
-  buttons?: string,
-  buttonBackground?:string
-}
-type Form={
-  key:string;
-  type:string;
-  required:boolean
-}
-type Action={
-  label:string; 
-  type:number;
-  end?:string;
-  link?:string;
-}
+type Params = {
+  id: string;
+};
+type Match = {
+  params: Params;
+};
+type Colors = {
+  main: string;
+  title: string;
+  background?: string;
+  description: string;
+  buttons?: string;
+  buttonBackground?: string;
+};
+type Form = {
+  key: string;
+  type: string;
+  required: boolean;
+};
+type Action = {
+  label: string;
+  type: number;
+  end?: string;
+  link?: string;
+};
 
-type Content={
-  actions :Array<Action>;
-  form:Array<Form>;
-  type:number;
-  message:string;
-}
-type Availability={
-  eventEndIdx:number;
-  afterEvents:Array<string>
-}
-type Events={
-  _id:string;
-  content:Content;
-  dependencies:Array<{availability:Availability}>;
-  title:string
-}
-type ApiData={
-  paths:Array<{events:Array<Events>;_id:string;}>;
-  colors:Colors;
-  title:string;
-  _id:string;
-}
+type Content = {
+  actions: Array<Action>;
+  form: Array<Form>;
+  type: number;
+  message: string;
+};
+type Availability = {
+  eventEndIdx: number;
+  afterEvents: Array<string>;
+};
+type Events = {
+  _id: string;
+  content: Content;
+  dependencies: Array<{ availability: Availability }>;
+  title: string;
+};
+type ApiData = {
+  paths: Array<{ events: Array<Events>; _id: string }>;
+  colors: Colors;
+  title: string;
+  _id: string;
+};
 
-export default function UserApp(props:FormComponentProps) {
-
+export default function UserApp(props: FormComponentProps) {
   const defaultColors = {
     main: "#44a785",
     title: "#ffffff",
@@ -67,31 +73,48 @@ export default function UserApp(props:FormComponentProps) {
     buttonBackground: "#ffffff",
   };
 
-  const [apiData, setApiData] = useState< ApiData|null>(null);
+  const [apiData, setApiData] = useState<ApiData | null>(null);
   let [currentPath, setCurrentPath] = useState<number>(0);
   let [currentEvent, setCurrentEvent] = useState<number>(0);
   let [apiColors, setApiColors] = useState<Colors>(defaultColors);
-  let [errorMessge,setErrorMessage]=useState<String>("")
+  let [errorMessge, setErrorMessage] = useState<string>("");
+  let [document, setDocument] = useState<string>("");
+  let [data, setData] = useState<Object>({});
   let refs = useRef<any>([]);
 
   let history = useHistory();
-  let actionNumber:number=0;
+  let actionNumber: number = 0;
 
-  useEffect(():void => {
-    function findFirstEvent(data:ApiData){
-        for (let i = 0; i < data.paths.length; i++)
-          for(let j=0;j<data.paths[i].events.length;j++){
-            if (
-                typeof data.paths[i].events[j].dependencies[0].availability
-                  .afterEvents ==='undefined'
-              ) {
-                setCurrentPath(i);
-                setCurrentEvent(j);
-                return;
-              }
+
+
+  useEffect((): void => {
+    async function addAnalytics() {
+    
+      const docRef = await addDoc(collection(db, "analytics-test"), {
+        events: [],
+        //add here id:random id
+        planID: props.match.params.id,
+        planVersion: 1,
+        startTime: Date.now(),
+      });
+      setDocument(docRef.id); //instead of docRef.id, pass the random id
+    }
+
+    function findFirstEvent(data: ApiData) {
+      for (let i = 0; i < data.paths.length; i++)
+        for (let j = 0; j < data.paths[i].events.length; j++) {
+          if (
+            typeof data.paths[i].events[j].dependencies[0].availability
+              .afterEvents === "undefined"
+          ) {
+            setCurrentPath(i);
+            setCurrentEvent(j);
+            return;
+          }
         }
     }
-    onSnapshot(doc(db, "planz", props.match.params.id), (doc:any) => {
+
+    onSnapshot(doc(db, "planz", props.match.params.id), (doc: any) => {
       if (doc.exists()) {
         setApiColors({
           background: "#ffffff",
@@ -99,16 +122,13 @@ export default function UserApp(props:FormComponentProps) {
           ...doc.data().colors,
         });
         setApiData(doc.data());
-        findFirstEvent(doc.data())
+        findFirstEvent(doc.data());
+        addAnalytics()
       } else history.push(`${props.match.params.id}/Registration`);
-   
     });
-    
-
   }, [history, props.match.params.id]);
 
-
-  function goToNextEvent(action:number) {
+  function goToNextEvent(action: number) {
     let nextEvent = currentEvent + 1;
     //check if the next event is on the same path
     for (
@@ -116,152 +136,172 @@ export default function UserApp(props:FormComponentProps) {
       i < apiData!.paths[currentPath].events.length;
       i++
     ) {
-      if(apiData!.paths[currentPath].events[i].dependencies)
-      for (
-        let k = 0;
-        k < apiData!.paths[currentPath].events[nextEvent].dependencies.length;
-        k++
-      )   
-      if (
-          apiData!.paths[currentPath].events[currentEvent]._id ===
-            apiData!.paths[currentPath].events[i].dependencies[k]
-              .availability.afterEvents[0] &&
-          action ===
-            apiData!.paths[currentPath].events[i].dependencies[k]
-              .availability.eventEndIdx
-        ) {
-          setCurrentEvent(i);
-          return;
-        }
+      if (apiData!.paths[currentPath].events[i].dependencies)
+        for (
+          let k = 0;
+          k < apiData!.paths[currentPath].events[nextEvent].dependencies.length;
+          k++
+        )
+          if (
+            apiData!.paths[currentPath].events[currentEvent]._id ===
+              apiData!.paths[currentPath].events[i].dependencies[k].availability
+                .afterEvents[0] &&
+            action ===
+              apiData!.paths[currentPath].events[i].dependencies[k].availability
+                .eventEndIdx
+          ) {
+            setCurrentEvent(i);
+            return;
+          }
     }
-  
 
     //check if the next event is on other path
-    console.log(apiData!.paths[currentPath].events[currentEvent]._id)
-
     for (let i = 0; i < apiData!.paths.length; i++)
-      for(let j=0;j<apiData!.paths[i].events.length;j++)
-        if(apiData!.paths[i].events[j].dependencies)
+      for (let j = 0; j < apiData!.paths[i].events.length; j++)
+        if (apiData!.paths[i].events[j].dependencies)
           for (
-                let k = 0;
-                k < apiData!.paths[i].events[j].dependencies.length;
-                k++
+            let k = 0;
+            k < apiData!.paths[i].events[j].dependencies.length;
+            k++
+          ) {
+            if (
+              apiData!.paths[i].events[j].dependencies[k].availability
+                .afterEvents
+            ) {
+              if (
+                apiData!.paths[currentPath].events[currentEvent]._id ===
+                  apiData!.paths[i].events[j].dependencies[k].availability
+                    .afterEvents[0] &&
+                action ===
+                  apiData!.paths[i].events[j].dependencies[k].availability
+                    .eventEndIdx
               ) {
-                if(apiData!.paths[i].events[j].dependencies[k].availability.afterEvents){
-                  if(apiData!.paths[currentPath].events[currentEvent]._id===
-                    apiData!.paths[i].events[j].dependencies[k].availability.afterEvents[0]
-                    &&action ===
-                    apiData!.paths[i].events[j].dependencies[k]
-                      .availability.eventEndIdx
-                  )
-                    {
-                        setCurrentPath(i);
-                        setCurrentEvent(j);
-                      }
-                }
+                setCurrentPath(i);
+                setCurrentEvent(j);
               }
-    
-    
-    
+            }
+          }
   }
-
+  //check if the event is type form
   function checkIfForm() {
     if (apiData!.paths[currentPath].events[currentEvent].content.type === 2)
       return true;
     return false;
   }
 
-function checkValidation():string{
-  var re = /\S+@\S+\.\S+/;
-  for(let i=0;i<refs.current.length;i++){
-    if(refs.current[i].required && refs.current[i].value==="")
-      return "יש למלא את כל השדות עם *"
-    else if(refs.current[i].type==='number' && refs.current[i].value.length!==10)
-      return "נא להזין פלאפון תקין"
-    else if(refs.current[i].type==='email' && re.test(refs.current[i].value)===false)   
-      return "נא להזין מייל תקין"
-    
+  //check validation of form
+  function checkValidation(): string {
+    var re = /\S+@\S+\.\S+/;
+    for (let i = 0; i < refs.current.length; i++) {
+      if (refs.current[i].required && refs.current[i].value === "")
+        return "יש למלא את כל השדות עם *";
+      else if (
+        refs.current[i].type === "number" &&
+        refs.current[i].value.length !== 10
+      )
+        return "נא להזין פלאפון תקין";
+      else if (
+        refs.current[i].type === "email" &&
+        re.test(refs.current[i].value) === false
+      )
+        return "נא להזין מייל תקין";
+    }
+    return "pass";
   }
-  return "pass"
-}
+  
+  function clearAll() {
+    setErrorMessage("");
+    refs.current = [];
+    setData({});
+  }
 
-function clearAll(){
-  setErrorMessage("")
-  refs.current=[]
-}
+  async function saveEvent(event: any) {
+    let eventData = {
+      endWith: event.target.outerText || 'Default',
+      eventId: apiData!.paths[currentPath].events[currentEvent]._id,
+      time: Date.now(),
+    };
+    if (Object.keys(data).length !== 0) Object.assign(eventData, { data });
+    await updateDoc(doc(db, "analytics-test", document), {
+      events: arrayUnion(eventData),
+    });
+  }
 
-  function toNextSlide(event:React.MouseEvent<HTMLButtonElement>):void {
-    event.preventDefault()
+  function changeHandler(event: any) {
+    const { name, value } = event.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function toNextSlide(event: React.MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
     let action = Number((event.target as HTMLButtonElement).id);
-    if (checkIfForm()){
-      let message:string=checkValidation()
-        if(message==="pass") {
-          setCurrentEvent(currentEvent + 1);
+    if (checkIfForm()) {
+      let message: string = checkValidation();
+      if (message === "pass") {
+        setCurrentEvent(currentEvent + 1);
+      } else {
+        setErrorMessage(message);
+        return;
       }
-      else{
-        setErrorMessage(message)
-        return
-      }
-    } 
-    else
-      goToNextEvent(action)
-    clearAll()
+    } else goToNextEvent(action);
+
+    saveEvent(event);
+    clearAll();
   }
+
   return (
     <div>
       <Helmet>
-                <style>{`body { background-color:  ${apiColors.background}; }`}</style>
-            </Helmet>
+        <style>{`body { background-color:  ${apiColors.background}; }`}</style>
+      </Helmet>
+
       <div className="event-modal" dir="rtl">
-      
         <div>
           <div
-            className="MuiPaper-root MuiDialog-paper MuiDialog-paperScrollPaper MuiDialog-paperWidthSm MuiPaper-elevation24 MuiPaper-rounded"
             aria-labelledby="customized-dialog-title"
           >
-            <div 
-            className="content rtl-heading"
-            >
-
-              <p
-                className="heading"
-                style={{ color: apiColors.main }}
-              >{apiData &&
+            <div className="content rtl-heading">
+              <p className="heading" style={{ color: apiColors.main }}>
+                {apiData &&
                   apiData.paths[currentPath].events[currentEvent].title}
               </p>
-         
-                <p className="content-message rtl-content-message" style={{color:apiColors.description}}>
-                { (apiData && apiData.paths[currentPath].events[currentEvent].content.message!=='.')
-                && apiData.paths[currentPath].events[currentEvent].content.message}
-                </p>
-        
+
+              <p
+                className="content-message rtl-content-message"
+                style={{ color: apiColors.description }}
+              >
+                {apiData &&
+                  apiData.paths[currentPath].events[currentEvent].content
+                    .message !== "." &&
+                  apiData.paths[currentPath].events[currentEvent].content
+                    .message}
+              </p>
             </div>
 
             <form className="jss21">
-            
               <div className="jss22">
-              
                 <div className="form-cont">
-                
                   {apiData &&
                     apiData.paths[currentPath].events[currentEvent].content
                       .type === 2 &&
                     apiData.paths[currentPath].events[
                       currentEvent
-                    ].content.form.map((input:Form,idx:any) => (
+                    ].content.form.map((input: Form, idx: any) => (
                       <div key={input.key}>
-                       
-                          
-                            <label htmlFor={input.key}>{input.key} {input.required&&'*'}</label><br/>
-                            <input
-                              type={input.type}
-                              id={input.key}
-                              required={input.required}
-                              aria-invalid="true"
-                              ref={(element) => {refs.current[idx] = element}}
-                            />  
-                         
-                        
+                        <label htmlFor={input.key}>
+                          {input.key} {input.required && "*"}
+                        </label>
+                        <br />
+                        <input
+                          type={input.type}
+                          name={input.key}
+                          required={input.required}
+                          aria-invalid="true"
+                          onChange={changeHandler}
+                          ref={(element) => {
+                            refs.current[idx] = element;
+                          }}
+                        />
                       </div>
                     ))}
                 </div>
@@ -271,28 +311,25 @@ function clearAll(){
                 {apiData &&
                   apiData.paths[currentPath].events[
                     currentEvent
-                  ].content.actions.map((action:Action) => (
+                  ].content.actions.map((action: Action) => (
                     <div key={action.label}>
                       {action.link ? (
-                          <button
-                            tabIndex={0}
-                            className="action-button"
-                            onClick={(e):void=>{
-                              e.preventDefault();
-                              window.open(`${action.link}`,'_parent');
-                              }}
-                            style={{
-                              color:apiColors.title,                                
-                              backgroundColor:
-                                apiData.colors.buttonBackground
-                                  ? apiColors.buttonBackground
-                                  : apiColors.background,
-                            }}
-                          >
-                          
-                            {action.label}
-                          
-                          </button>
+                        <button
+                          tabIndex={0}
+                          className="action-button"
+                          onClick={(e): void => {
+                            e.preventDefault();
+                            window.open(`${action.link}`, "_parent");
+                          }}
+                          style={{
+                            color: apiColors.title,
+                            backgroundColor: apiData.colors.buttonBackground
+                              ? apiColors.buttonBackground
+                              : apiColors.background,
+                          }}
+                        >
+                          {action.label}
+                        </button>
                       ) : (
                         <button
                           tabIndex={0}
@@ -300,16 +337,13 @@ function clearAll(){
                           className="action-button"
                           onClick={(event) => toNextSlide(event)}
                           style={{
-                            color:apiColors.title,
-                            backgroundColor:
-                              apiData.colors.buttonBackground
-                                ? apiColors.buttonBackground
-                                : apiColors.background
+                            color: apiColors.title,
+                            backgroundColor: apiData.colors.buttonBackground
+                              ? apiColors.buttonBackground
+                              : apiColors.background,
                           }}
                         >
-                    
-                            {action.label}
-                       
+                          {action.label}
                         </button>
                       )}
                     </div>
@@ -320,11 +354,6 @@ function clearAll(){
           </div>
         </div>
       </div>
-
-      
-      {/* <iframe src="http://localhost:3000/01849380-cbad-442e-96bb-a5527709529b" title="yoni" width="500" height="450" frameBorder="0"></iframe> */}
-
-
     </div>
   );
 }
